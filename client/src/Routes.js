bc84485fceb8d820de27b08972bd582c0b0a38e9
Logician724 <reda.i@eseed.net>
2019-07-31 08:09:10 +0200

    feat: Erase extra sidenav items

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -24,7 +24,7 @@ const Routes = () => {
       <Redirect
         exact
         from="/"
-        to="/dashboard"
+        to="/products"
       />
       <RouteWithLayout
         component={DashboardView}
